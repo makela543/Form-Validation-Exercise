f8c924023f776f637e3fc864107a236958d6c5d0
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-09-13 14:25:45 +0300

    Setup validation system for the email and phone number fields

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
     
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+    
+    
+    $("#submitButton").click(function() {
+        
+        var errorMessage = "";
+        
+        if (isEmail($("#email").val()) == false) {
+            
+            errorMessage += "<p>Your email address is not valid.</p>";
+            
+        }
+        
+        if ($.isNumeric($("phone").val()) == false) {
+            
+            errorMessage += "<p>Your phone number is not numeric.</p>";
+            
+        }
+        
+    });
+    
+    
     
 });
