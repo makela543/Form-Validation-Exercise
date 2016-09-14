7bfeecada8fde4045d5a85957a0972a8a1b1fcc1
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-09-14 16:10:12 +0300

    Setup validation to make sure passwords match and that all fields are filled. Add success message

--- script.js
+++ script.js
@@ -9,21 +9,49 @@ $(document).ready(function() {
     $("#submitButton").click(function() {
         
         var errorMessage = "";
+        var fieldsMissing = "";
         
-        if (isEmail($("#email").val()) == false) {
-            
-            errorMessage += "<p>Your email address is not valid.</p>";
-            
+        if ($("#email").val() == "") {
+            fieldsMissing += "<br>Email";
         }
         
-        if ($.isNumeric($("phone").val()) == false) {
-            
-            errorMessage += "<p>Your phone number is not numeric.</p>";
-            
+        if ($("#phone").val() == "") {
+            fieldsMissing += "<br>Phone";
+        }
+        
+        if ($("#password").val() == "") {
+            fieldsMissing += "<br>Password";
+        }
+        
+        if ($("#passwordConfirm").val() == "") {
+            fieldsMissing += "<br>Confirm Password";
+        }
+        
+        if (fieldsMissing != "") {
+            errorMessage += "The following fields are missing:" + fieldsMissing;
+        }
+        
+        if (isEmail($("#email").val()) == false) {
+            errorMessage += "<p>Your email address is not valid.</p>";
+        }
+        
+        if ($.isNumeric($("#phone").val()) == false) {
+            errorMessage += "<p>Your phone number is not numeric.</p>"; 
+        }
+        
+        if ($("#password").val() != $("#passwordConfirm").val()) {
+            errorMessage += "<p>The passwords don't match.</p>";
+        }
+        
+        if (errorMessage != "") {
+            $("#errorMessage").css("display", "block");
+            $("#success").css("display", "none");
+            $("#errorMessage").html(errorMessage);
+        } else {
+            $("#errorMessage").css("display", "none");
+            $("#success").css("display", "block");
         }
         
     });
-    
-    
     
 });
